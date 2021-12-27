6fa4f3c4552241075568136671a274b3a4b49585
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-12-27 16:50:11 +0600

    progress made

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const app = Vue.createApp({
         attackMoster() {
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            console.log(`monster health == ${this.monsterHealth}`);
             this.attackPlayer();
         },
 
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth = this.playerHealth - attackValue;
+            console.log(`player health == ${this.playerHealth}`);
         }
     }
 });
