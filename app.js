e61f7d2a0b9f87ea4b66143ff8b1fa045eb8aa9a
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-12-27 16:14:33 +0600

    progess made

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const app = Vue.createApp({});
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        }
+    },
+
+    methods: {
+        attackMoster() {
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
+        },
+
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth = this.playerHealth - attackValue;
+        }
+    }
+});
 
 app.mount('#game');
