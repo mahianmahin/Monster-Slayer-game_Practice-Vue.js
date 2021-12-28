55efde006eadc3293ddd6f9acbf20fcd26ecad33
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-12-28 10:28:59 +0600

    progress made

--- app.js
+++ app.js
@@ -9,16 +9,53 @@ const app = Vue.createApp({
             monsterHealth: 100,
             specialAttackCounter: 0,
             healCounter: 0,
+            winner: null,
+            gameRunning: true,
+            logMessages: []
+        }
+    },
+
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                // Draw
+                this.winner = 'draw';
+                this.gameRunning = false;
+            } else if (value <= 0) {
+                // Player lost
+                this.winner = 'monster';
+                this.gameRunning = false;
+            }
+        },
+
+        monsterHealth(value) {
+            if (value <= 0 && this, this.playerHealth <= 0) {
+                // Draw
+                this.winner = 'draw';
+                this.gameRunning = false;
+            } else if (value <= 0) {
+                // monster lost
+                this.winner = 'player';
+                this.gameRunning = false;
+            }
         }
     },
 
     computed: {
         monsterBarStyle() {
-            return { width: this.monsterHealth + '%' };
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            } else {
+                return { width: this.monsterHealth + '%' };
+            }
         },
 
         playerBarStyle() {
-            return { width: this.playerHealth + '%' };
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            } else {
+                return { width: this.playerHealth + '%' };
+            }
         },
 
         specialAttackPermission() {
@@ -39,23 +76,36 @@ const app = Vue.createApp({
     },
 
     methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.specialAttackCounter = 0;
+            this.healCounter = 0;
+            this.winner = null;
+            this.gameRunning = true;
+            this.logMessages = [];
+        },
+
         attackMoster() {
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth = this.monsterHealth - attackValue;
             this.specialAttackCounter += 1;
             this.healCounter += 1;
+            this.addLogMessages('player', 'attack', attackValue);
             this.attackPlayer();
         },
 
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth = this.playerHealth - attackValue;
+            this.addLogMessages('monster', 'attack', attackValue);
         },
 
         specialAttackMonster() {
             const attackValue = getRandomValue(15, 35);
             this.monsterHealth = this.monsterHealth - attackValue;
             this.specialAttackCounter = 0;
+            this.addLogMessages('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
 
@@ -69,9 +119,22 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue;
             }
-
+            this.addLogMessages('player', 'heal', healValue);
             this.attackPlayer();
         },
+
+        surrender() {
+            this.winner = 'monster';
+            this.gameRunning = false;
+        },
+
+        addLogMessages(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
+        }
     }
 });
 
